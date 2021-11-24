8e44a5ced9379c4b5667be3ea653b944097095d9
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-24 21:00:52 +0900

    ADD : RecyclePostAdapter, DiffUtilCallBack

---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/adapter/RecyclePostAdapter.kt
@@ -0,0 +1,38 @@
+package kr.hs.dgsw.jobdongsani_android.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kr.hs.dgsw.jobdongsani_android.R
+import kr.hs.dgsw.jobdongsani_android.adapter.callback.RecyclePostDiffUtilCallBack
+import kr.hs.dgsw.jobdongsani_android.databinding.ItemRecyclePostBinding
+import kr.hs.dgsw.jobdongsani_android.util.SingleLiveEvent
+
+class RecyclePostAdapter :
+    ListAdapter<Any, RecyclePostAdapter.RecyclePostViewHolder>(RecyclePostDiffUtilCallBack) {
+
+    class RecyclePostViewHolder(val binding: ItemRecyclePostBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclePostViewHolder {
+        RecyclePostAdapter.RecyclePostViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.item_recycle_post,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclePostViewHolder, position: Int) {
+
+    }
+    companion object {
+        val onClickDelete = SingleLiveEvent<Unit>()
+    }
+}
