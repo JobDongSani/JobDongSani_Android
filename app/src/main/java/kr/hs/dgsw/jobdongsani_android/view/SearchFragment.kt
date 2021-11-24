4ce284a0088b31bd5a6d8e57e2911ea8406b2e2c
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-24 19:35:31 +0900

    [ADD] SearchFragment

--- app/src/main/java/kr/hs/dgsw/jobdongsani_android/view/SearchFragment.kt
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/view/SearchFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import kr.hs.dgsw.jobdongsani_android.R
+import kr.hs.dgsw.jobdongsani_android.adapter.SearchPostAdapter
+import kr.hs.dgsw.jobdongsani_android.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentSearchBinding>(inflater,
+            R.layout.fragment_search,
+            container,
+            false)
+        binding.lifecycleOwner = this
+
+        val searchPostAdapter = SearchPostAdapter().apply {
+            submitList(listOf(Any(), Any(), Any(), Any(), Any(), Any(), Any()))
+        }
+
+        binding.rvSearchPost.adapter = searchPostAdapter
+
+
+        return binding.root
     }
 }
