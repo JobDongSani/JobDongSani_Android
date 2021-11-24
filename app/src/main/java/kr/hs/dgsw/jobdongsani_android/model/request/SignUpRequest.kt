ba6bd4f013c31b4252e9df89139322b39fa457e7
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-25 08:34:10 +0900

    [FIX] API

--- app/src/main/java/kr/hs/dgsw/jobdongsani_android/model/request/SignUpRequest.kt
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/model/request/SignUpRequest.kt
@@ -4,5 +4,6 @@ data class SignUpRequest(
     val username : String,      // id
     val password : String,
     val name : String,
-    val phonenumber : String
+    val phonenumber : String,
+    val userImage: String
 )
