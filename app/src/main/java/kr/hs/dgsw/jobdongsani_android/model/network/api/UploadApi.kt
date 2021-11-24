cb97dbfe9b58c233e1df2cdad3b1967c7b2f251f
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-24 19:39:20 +0900

    ADD : UploadAPI

---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/model/network/api/UploadApi.kt
@@ -0,0 +1,18 @@
+package kr.hs.dgsw.jobdongsani_android.model.network.api
+
+import io.reactivex.Single
+import kr.hs.dgsw.jobdongsani_android.model.response.UploadResponse
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import java.io.File
+
+interface UploadApi {
+
+    @Multipart
+    @POST("/file")
+    fun uploadImage(
+        @Part file : MultipartBody.Part
+    ) : Single<UploadResponse>
+}
---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/model/response/UploadResponse.kt
@@ -0,0 +1,5 @@
+package kr.hs.dgsw.jobdongsani_android.model.response
+
+data class UploadResponse(
+    val imageUrl : String
+)
