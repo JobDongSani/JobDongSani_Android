be5c1ec6b0f48b707b93a71d1a3c7eea2a8bf453
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-24 13:52:00 +0900

    [ADD] BaseCode

---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/base/BaseViewModel.kt
@@ -0,0 +1,48 @@
+package kr.hs.dgsw.jobdongsani_android.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val disposable = CompositeDisposable()
+    val onErrorEvent = MutableLiveData<Throwable>()
+
+    private val isLoading = MutableLiveData<Boolean>()
+    val getIsLoading: LiveData<Boolean> = isLoading
+
+    fun <T> addDisposable(
+        single: Single<T>,
+        onSuccess: (T) -> Unit,
+        onFailure: (Throwable) -> Unit,
+    ) {
+        disposable.add(
+            single.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(onSuccess, onFailure)
+        )
+    }
+
+    fun <T> addDisposable(
+        completable: Completable,
+        onSuccess: () -> Unit,
+        onFailure: (Throwable) -> Unit,
+    ) {
+        disposable.add(
+            completable.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(onSuccess, onFailure)
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+}
