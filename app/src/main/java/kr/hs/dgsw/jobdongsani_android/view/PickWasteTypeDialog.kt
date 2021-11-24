78829eacf29f653c2a8483f431e627b9d596c2d4
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-25 05:19:25 +0900

    [ADD] Pick Waste Type Dialog & Adapter

---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/view/PickWasteTypeDialog.kt
@@ -0,0 +1,55 @@
+package kr.hs.dgsw.jobdongsani_android.view
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import kr.hs.dgsw.jobdongsani_android.R
+import kr.hs.dgsw.jobdongsani_android.adapter.PickWasteTypeAdapter
+import kr.hs.dgsw.jobdongsani_android.databinding.DialogPickWasteTypeBinding
+import kr.hs.dgsw.jobdongsani_android.viewmodel.PickWasteTypeViewModel
+
+class PickWasteTypeDialog(val barcode: String) : DialogFragment() {
+
+    val viewModel: PickWasteTypeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding = DataBindingUtil.inflate<DialogPickWasteTypeBinding>(inflater,
+            R.layout.dialog_pick_waste_type,
+            container,
+            false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val pickWasteTypeAdapter = PickWasteTypeAdapter().apply {
+            submitList(listOf("종이류", "플라스틱류", "고철류", "의 류", "캔 류", "BOX류", "비닐류"))
+        }
+
+        PickWasteTypeAdapter.onClick.observe(this, {
+            viewModel.postProduct(barcode, it)
+        })
+
+        viewModel.getIsLoading().observe(this, {
+            dismiss()
+        })
+        viewModel.onErrorEvent.observe(this, {
+            Toast.makeText(requireContext(), "${it.message}", Toast.LENGTH_SHORT).show()
+        })
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        binding.rvWasteType.adapter = pickWasteTypeAdapter
+
+        isCancelable = false
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/viewmodel/PickWasteTypeViewModel.kt
@@ -0,0 +1,20 @@
+package kr.hs.dgsw.jobdongsani_android.viewmodel
+
+import kr.hs.dgsw.jobdongsani_android.base.BaseViewModel
+import kr.hs.dgsw.jobdongsani_android.model.network.Server.productApi
+import kr.hs.dgsw.jobdongsani_android.model.request.ProductRequest
+
+class PickWasteTypeViewModel : BaseViewModel() {
+
+    fun postProduct(barcode: String, type: String) {
+        addDisposable(
+            productApi.postProduct(ProductRequest(barcode, type)), {
+                isLoading.value = false
+            }, {
+                isLoading.value = false
+                onErrorEvent.value = it
+            }
+        )
+    }
+
+}
