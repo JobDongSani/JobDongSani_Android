f6fa85fa0aaa9c47060ef256d8ea2745401695c3
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-24 20:08:32 +0900

    [ADD] PostDetail

---
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/view/PostDetailFragment.kt
@@ -0,0 +1,20 @@
+package kr.hs.dgsw.jobdongsani_android.view
+
+import androidx.fragment.app.viewModels
+import kr.hs.dgsw.jobdongsani_android.base.BaseFragment
+import kr.hs.dgsw.jobdongsani_android.databinding.FragmentPostDetailBinding
+import kr.hs.dgsw.jobdongsani_android.viewmodel.PostDetailViewModel
+
+class PostDetailFragment : BaseFragment<FragmentPostDetailBinding, PostDetailViewModel>() {
+
+    override val viewModel: PostDetailViewModel by viewModels()
+
+    override val isBottomNavFragment: Boolean = false
+
+    override fun observerViewModel() {
+        with(viewModel) {
+
+        }
+    }
+
+}
