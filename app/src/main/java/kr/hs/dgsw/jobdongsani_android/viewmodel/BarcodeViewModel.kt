af3ad32e8bc3b6c3d6a1405ca56e81b05602af54
사승은 <62982576+Silver-Deer@users.noreply.github.com>
2021-11-25 10:28:45 +0900

    [ADD] Save Product

--- app/src/main/java/kr/hs/dgsw/jobdongsani_android/viewmodel/BarcodeViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/viewmodel/BarcodeViewModel.kt
@@ -2,7 +2,8 @@ package kr.hs.dgsw.jobdongsani_android.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import kr.hs.dgsw.jobdongsani_android.base.BaseViewModel
-import kr.hs.dgsw.jobdongsani_android.model.network.NaverAPI.naverApi
+import kr.hs.dgsw.jobdongsani_android.model.local.AppDatabase
+import kr.hs.dgsw.jobdongsani_android.model.local.entity.ProductEntity
 import kr.hs.dgsw.jobdongsani_android.model.response.Product
 import kr.hs.dgsw.jobdongsani_android.repository.BarcodeRepository
 
@@ -19,6 +20,20 @@ class BarcodeViewModel : BaseViewModel() {
                 onErrorEvent.value = it
             }
         )
+    }
+
+    fun saveProduct(product: Product) {
+        AppDatabase.instance?.productDao?.let {
+            addDisposable(
+                it.insertProduct(ProductEntity(product.barcode,
+                    product.title,
+                    product.wasteType,
+                    product.way)), {
+
+                }, { t ->
+                    onErrorEvent.value = t
+                })
+        }
     }
 
 }
