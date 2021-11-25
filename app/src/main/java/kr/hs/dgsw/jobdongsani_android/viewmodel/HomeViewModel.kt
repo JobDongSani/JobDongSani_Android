4a92082f9b91ee064877c48adad760b703961c62
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-25 09:04:42 +0900

    ADD : HomeFragment -> WritePostFragment, Barcode Activity Event
    and refactor code

--- app/src/main/java/kr/hs/dgsw/jobdongsani_android/viewmodel/HomeViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/jobdongsani_android/viewmodel/HomeViewModel.kt
@@ -1,7 +1,19 @@
 package kr.hs.dgsw.jobdongsani_android.viewmodel
 
+import android.view.View
 import kr.hs.dgsw.jobdongsani_android.base.BaseViewModel
+import kr.hs.dgsw.jobdongsani_android.util.SingleLiveEvent
 
 class HomeViewModel: BaseViewModel() {
 
+    val onWritePostClickEvent = SingleLiveEvent<Unit>()
+    val onClickBarcodeEvent = SingleLiveEvent<Unit>()
+
+    fun onClickWritePost(view: View?) {
+        onWritePostClickEvent.call()
+    }
+
+    fun onClickBarcode(view: View?) {
+        onClickBarcodeEvent.call()
+    }
 }
